6b3008f451216c4cd3157d2fa51780bf4a5e6151
Christopher Toumanian <cctoumanian@gmail.com>
2020-10-30 19:00:30 -0700

    Adjusted spaces

--- src/channel.h
+++ src/channel.h
@@ -13,13 +13,13 @@ public:
     Channel();
 
 	std::string name = "channel";
-    std::vector<User*> users;
+	std::vector<User*> users;
 	int current_id = 0;
-    Server* server;
+	Server* server;
     
 	virtual void load();
 	virtual void update();
-    void receive(int user_id, std::string message);
+	void receive(int user_id, std::string message);
 
 	virtual void user_connected(User* user);
 	virtual void user_disconnected(User* user);
@@ -27,14 +27,14 @@ public:
 	virtual void send_to_all(std::string message);
 	virtual void send_to_user(User* user, std::string message);
 
-    User* get_user(int user_id);
-    User* get_user(std::string username);
-    int get_users_online();
-    User* add_user(int user_id);
-    void remove_user(User* user);
+	User* get_user(int user_id);
+	User* get_user(std::string username);
+	int get_users_online();
+	User* add_user(int user_id);
+	void remove_user(User* user);
 
 private:
-   	int get_new_id();
+	int get_new_id();
 };
 
 #endif
